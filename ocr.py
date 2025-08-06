debadae25bfe99c54d6124dab2254751f462288a
MzaCoding <nyikogiven74@gmail.com>
2025-08-06 13:38:16 +0200

    adding ocr function to extract text on image

---
+++ ocr.py
@@ -0,0 +1,34 @@
+import cv2 
+import pytesseract
+
+ 
+
+def ocr_core(img):
+    text=pytesseract.image_to_string(img)
+    return text
+
+
+img=cv2.imread('text.png')
+
+def get_grayscale(image):
+    return cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
+
+
+def threshholding(image):
+    return cv2.threshold(image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+
+img=get_grayscale(img)
+img=threshholding(img)
+img=remove_noise(img)
+
+
+text=ocr_core(img)
+
+if text =="":
+ print("no Text was found")
+
+else :
+ print(text)
